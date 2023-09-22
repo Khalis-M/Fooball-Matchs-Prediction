1389a2a3c47831b0340ba07ef18f01e3c243becf
Khalis-M <khalismehafed@gmail.com>
2023-09-22 22:31:29 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,429 @@
+const data = `Crystal Palace 0.89 - 1.87 Fulham
+Crystal Palace 1.14 - 1.03 Bournemouth
+Crystal Palace 2.09 - 1.42 Leeds
+Crystal Palace 1.4 - 1.93 Leicester
+Crystal Palace 1.08 - 2.15 Newcastle
+Crystal Palace 0.94 - 2.3 Tottenham
+Crystal Palace 1.69 - 1.07 Everton
+Crystal Palace 1.15 - 1.48 Manchester United
+Crystal Palace 1.67 - 1.84 West Ham
+Crystal Palace 1.96 - 1.09 Aston Villa
+Crystal Palace 0.82 - 1.41 Brentford
+Crystal Palace 1.27 - 1.97 Brighton
+Crystal Palace 1.21 - 1.27 Chelsea
+Crystal Palace 0.8 - 0.95 Liverpool
+Crystal Palace 0.46 - 1.67 Manchester City
+Crystal Palace 1.55 - 0.83 Nottingham Forest
+Crystal Palace 1.49 - 0.8 Southampton
+Crystal Palace 1.75 - 1.08 Wolves
+Crystal Palace 0.7 - 1.91 Arsenal
+Fulham 1.33 - 1.25 Crystal Palace
+Fulham 1.23 - 0.75 Bournemouth
+Fulham 1.99 - 1.34 Leeds
+Fulham 2.25 - 1.95 Leicester
+Fulham 1.31 - 1.94 Newcastle
+Fulham 1.28 - 1.61 Tottenham
+Fulham 1.21 - 0.72 Everton
+Fulham 1.3 - 1.31 Manchester United
+Fulham 1.04 - 1.34 West Ham
+Fulham 1.74 - 0.76 Aston Villa
+Fulham 1.0 - 1.32 Brentford
+Fulham 1.35 - 1.84 Brighton
+Fulham 1.41 - 1.21 Chelsea
+Fulham 1.4 - 1.27 Liverpool
+Fulham 0.71 - 1.72 Manchester City
+Fulham 1.56 - 0.71 Nottingham Forest
+Fulham 1.56 - 0.84 Southampton
+Fulham 1.68 - 0.86 Wolves
+Fulham 0.88 - 1.75 Arsenal
+Bournemouth 0.92 - 1.33 Crystal Palace
+Bournemouth 0.93 - 1.39 Fulham
+Bournemouth 2.63 - 1.17 Leeds
+Bournemouth 1.48 - 1.43 Leicester
+Bournemouth 1.03 - 1.83 Newcastle
+Bournemouth 1.07 - 1.66 Tottenham
+Bournemouth 1.94 - 0.88 Everton
+Bournemouth 1.0 - 1.19 Manchester United
+Bournemouth 0.67 - 2.03 West Ham
+Bournemouth 1.59 - 1.14 Aston Villa
+Bournemouth 0.69 - 1.08 Brentford
+Bournemouth 0.97 - 1.73 Brighton
+Bournemouth 0.99 - 1.21 Chelsea
+Bournemouth 0.99 - 0.86 Liverpool
+Bournemouth 0.37 - 1.73 Manchester City
+Bournemouth 1.36 - 0.93 Nottingham Forest
+Bournemouth 1.74 - 1.16 Southampton
+Bournemouth 1.92 - 1.31 Wolves
+Bournemouth 0.43 - 1.83 Arsenal
+Leeds 1.36 - 0.95 Crystal Palace
+Leeds 1.34 - 1.78 Fulham
+Leeds 1.24 - 0.85 Bournemouth
+Leeds 1.77 - 1.57 Leicester
+Leeds 1.48 - 2.04 Newcastle
+Leeds 1.64 - 2.3 Tottenham
+Leeds 1.45 - 0.98 Everton
+Leeds 1.7 - 1.42 Manchester United
+Leeds 1.44 - 1.25 West Ham
+Leeds 1.59 - 0.91 Aston Villa
+Leeds 0.78 - 1.18 Brentford
+Leeds 1.87 - 2.09 Brighton
+Leeds 1.82 - 0.95 Chelsea
+Leeds 1.75 - 1.4 Liverpool
+Leeds 0.93 - 1.89 Manchester City
+Leeds 1.73 - 0.61 Nottingham Forest
+Leeds 1.52 - 0.66 Southampton
+Leeds 1.83 - 0.8 Wolves
+Leeds 1.12 - 1.96 Arsenal
+Leicester 0.72 - 0.88 Crystal Palace
+Leicester 0.5 - 1.14 Fulham
+Leicester 0.58 - 1.05 Bournemouth
+Leicester 1.96 - 1.05 Leeds
+Leicester 1.47 - 1.31 Newcastle
+Leicester 2.06 - 1.4 Tottenham
+Leicester 1.58 - 1.32 Everton
+Leicester 1.21 - 1.18 Manchester United
+Leicester 1.1 - 1.05 West Ham
+Leicester 1.36 - 1.27 Aston Villa
+Leicester 0.76 - 1.19 Brentford
+Leicester 1.59 - 1.56 Brighton
+Leicester 1.28 - 1.56 Chelsea
+Leicester 1.66 - 1.16 Liverpool
+Leicester 0.49 - 1.34 Manchester City
+Leicester 1.11 - 0.95 Nottingham Forest
+Leicester 1.13 - 1.03 Southampton
+Leicester 1.43 - 1.11 Wolves
+Leicester 0.61 - 1.45 Arsenal
+Newcastle 2.07 - 0.53 Crystal Palace
+Newcastle 1.92 - 0.71 Fulham
+Newcastle 2.78 - 0.64 Bournemouth
+Newcastle 1.83 - 0.68 Leeds
+Newcastle 3.41 - 1.16 Leicester
+Newcastle 2.42 - 1.29 Tottenham
+Newcastle 1.76 - 0.4 Everton
+Newcastle 2.64 - 0.45 Manchester United
+Newcastle 2.33 - 0.93 West Ham
+Newcastle 2.57 - 0.36 Aston Villa
+Newcastle 2.57 - 1.07 Brentford
+Newcastle 2.25 - 1.62 Brighton
+Newcastle 1.67 - 0.51 Chelsea
+Newcastle 1.62 - 1.31 Liverpool
+Newcastle 1.99 - 1.89 Manchester City
+Newcastle 2.56 - 0.37 Nottingham Forest
+Newcastle 2.84 - 0.6 Southampton
+Newcastle 3.09 - 0.34 Wolves
+Newcastle 2.01 - 1.77 Arsenal
+Tottenham 2.03 - 1.08 Crystal Palace
+Tottenham 1.98 - 1.56 Fulham
+Tottenham 2.88 - 1.49 Bournemouth
+Tottenham 2.69 - 1.85 Leeds
+Tottenham 3.86 - 1.91 Leicester
+Tottenham 1.46 - 1.27 Newcastle
+Tottenham 2.12 - 1.17 Everton
+Tottenham 2.74 - 1.22 Manchester United
+Tottenham 2.01 - 0.95 West Ham
+Tottenham 2.34 - 1.08 Aston Villa
+Tottenham 2.13 - 1.84 Brentford
+Tottenham 1.94 - 1.52 Brighton
+Tottenham 1.85 - 1.27 Chelsea
+Tottenham 2.12 - 0.88 Liverpool
+Tottenham 1.58 - 1.07 Manchester City
+Tottenham 2.71 - 0.93 Nottingham Forest
+Tottenham 3.08 - 1.11 Southampton
+Tottenham 2.67 - 0.7 Wolves
+Tottenham 1.87 - 1.6 Arsenal
+Everton 0.86 - 1.36 Crystal Palace
+Everton 0.76 - 2.02 Fulham
+Everton 0.97 - 0.97 Bournemouth
+Everton 1.74 - 1.65 Leeds
+Everton 0.96 - 1.88 Leicester
+Everton 0.87 - 2.63 Newcastle
+Everton 0.83 - 1.97 Tottenham
+Everton 0.89 - 1.65 Manchester United
+Everton 0.77 - 1.74 West Ham
+Everton 1.04 - 1.35 Aston Villa
+Everton 0.76 - 1.53 Brentford
+Everton 0.92 - 2.4 Brighton
+Everton 0.76 - 1.26 Chelsea
+Everton 0.68 - 1.24 Liverpool
+Everton 0.47 - 2.17 Manchester City
+Everton 1.17 - 0.98 Nottingham Forest
+Everton 1.11 - 1.38 Southampton
+Everton 1.24 - 1.45 Wolves
+Everton 0.62 - 1.8 Arsenal
+Manchester United 1.53 - 0.65 Crystal Palace
+Manchester United 1.59 - 0.91 Fulham
+Manchester United 2.37 - 0.56 Bournemouth
+Manchester United 1.89 - 1.2 Leeds
+Manchester United 3.33 - 0.83 Leicester
+Manchester United 0.99 - 0.64 Newcastle
+Manchester United 1.98 - 0.82 Tottenham
+Manchester United 1.61 - 0.89 Everton
+Manchester United 1.84 - 0.67 West Ham
+Manchester United 1.88 - 0.53 Aston Villa
+Manchester United 2.1 - 1.01 Brentford
+Manchester United 1.7 - 1.35 Brighton
+Manchester United 1.68 - 0.76 Chelsea
+Manchester United 1.74 - 0.92 Liverpool
+Manchester United 1.59 - 1.06 Manchester City
+Manchester United 2.11 - 0.44 Nottingham Forest
+Manchester United 2.72 - 0.59 Southampton
+Manchester United 2.98 - 0.39 Wolves
+Manchester United 1.95 - 1.19 Arsenal
+West Ham 1.1 - 1.12 Crystal Palace
+West Ham 1.04 - 1.08 Fulham
+West Ham 1.29 - 0.39 Bournemouth
+West Ham 2.4 - 1.26 Leeds
+West Ham 1.9 - 1.67 Leicester
+West Ham 1.21 - 2.56 Newcastle
+West Ham 1.29 - 1.78 Tottenham
+West Ham 1.59 - 0.62 Everton
+West Ham 1.23 - 0.83 Manchester United
+West Ham 1.38 - 0.72 Aston Villa
+West Ham 0.76 - 1.47 Brentford
+West Ham 1.34 - 1.92 Brighton
+West Ham 1.19 - 0.99 Chelsea
+West Ham 1.28 - 1.57 Liverpool
+West Ham 0.46 - 2.06 Manchester City
+West Ham 2.24 - 0.37 Nottingham Forest
+West Ham 1.53 - 0.52 Southampton
+West Ham 1.94 - 0.55 Wolves
+West Ham 0.73 - 1.94 Arsenal
+Aston Villa 1.37 - 0.59 Crystal Palace
+Aston Villa 1.56 - 1.06 Fulham
+Aston Villa 2.05 - 0.51 Bournemouth
+Aston Villa 2.14 - 1.15 Leeds
+Aston Villa 2.3 - 2.01 Leicester
+Aston Villa 1.94 - 1.23 Newcastle
+Aston Villa 2.02 - 1.5 Tottenham
+Aston Villa 1.92 - 0.94 Everton
+Aston Villa 2.14 - 1.14 Manchester United
+Aston Villa 1.11 - 0.96 West Ham
+Aston Villa 2.16 - 1.02 Brentford
+Aston Villa 2.02 - 1.63 Brighton
+Aston Villa 1.18 - 1.39 Chelsea
+Aston Villa 1.86 - 1.28 Liverpool
+Aston Villa 1.15 - 1.7 Manchester City
+Aston Villa 2.11 - 0.37 Nottingham Forest
+Aston Villa 1.72 - 0.5 Southampton
+Aston Villa 1.74 - 0.64 Wolves
+Aston Villa 1.66 - 2.07 Arsenal
+Brentford 1.71 - 1.0 Crystal Palace
+Brentford 2.08 - 1.5 Fulham
+Brentford 2.41 - 0.47 Bournemouth
+Brentford 2.91 - 1.49 Leeds
+Brentford 3.03 - 1.41 Leicester
+Brentford 1.28 - 1.94 Newcastle
+Brentford 2.38 - 1.64 Tottenham
+Brentford 2.17 - 0.83 Everton
+Brentford 2.8 - 0.92 Manchester United
+Brentford 2.03 - 1.27 West Ham
+Brentford 2.36 - 0.9 Aston Villa
+Brentford 2.04 - 1.31 Brighton
+Brentford 1.78 - 1.04 Chelsea
+Brentford 1.98 - 1.03 Liverpool
+Brentford 1.46 - 1.32 Manchester City
+Brentford 2.59 - 0.62 Nottingham Forest
+Brentford 2.89 - 0.7 Southampton
+Brentford 2.93 - 0.86 Wolves
+Brentford 1.44 - 1.66 Arsenal
+Brighton 1.15 - 0.62 Crystal Palace
+Brighton 1.09 - 1.11 Fulham
+Brighton 2.11 - 0.71 Bournemouth
+Brighton 1.42 - 1.09 Leeds
+Brighton 3.16 - 1.71 Leicester
+Brighton 0.73 - 1.07 Newcastle
+Brighton 1.41 - 1.58 Tottenham
+Brighton 1.12 - 1.89 Everton
+Brighton 1.69 - 0.7 Manchester United
+Brighton 1.52 - 0.95 West Ham
+Brighton 1.53 - 0.89 Aston Villa
+Brighton 1.86 - 2.0 Brentford
+Brighton 1.22 - 1.1 Chelsea
+Brighton 1.44 - 1.46 Liverpool
+Brighton 1.33 - 1.82 Manchester City
+Brighton 1.62 - 0.44 Nottingham Forest
+Brighton 2.57 - 0.82 Southampton
+Brighton 3.14 - 0.36 Wolves
+Brighton 1.45 - 2.3 Arsenal
+Chelsea 1.11 - 0.89 Crystal Palace
+Chelsea 0.71 - 0.86 Fulham
+Chelsea 1.39 - 0.61 Bournemouth
+Chelsea 2.04 - 1.31 Leeds
+Chelsea 1.77 - 1.48 Leicester
+Chelsea 1.09 - 1.29 Newcastle
+Chelsea 1.4 - 1.58 Tottenham
+Chelsea 1.47 - 1.14 Everton
+Chelsea 1.23 - 1.04 Manchester United
+Chelsea 0.96 - 0.97 West Ham
+Chelsea 1.27 - 1.17 Aston Villa
+Chelsea 0.73 - 1.43 Brentford
+Chelsea 1.16 - 1.67 Brighton
+Chelsea 1.15 - 1.16 Liverpool
+Chelsea 0.47 - 1.44 Manchester City
+Chelsea 1.72 - 0.64 Nottingham Forest
+Chelsea 1.57 - 0.76 Southampton
+Chelsea 2.14 - 0.49 Wolves
+Chelsea 0.65 - 1.63 Arsenal
+Liverpool 2.06 - 0.82 Crystal Palace
+Liverpool 1.81 - 0.65 Fulham
+Liverpool 4.23 - 0.51 Bournemouth
+Liverpool 1.99 - 1.17 Leeds
+Liverpool 3.79 - 1.23 Leicester
+Liverpool 1.47 - 1.11 Newcastle
+Liverpool 3.22 - 1.69 Tottenham
+Liverpool 1.96 - 0.51 Everton
+Liverpool 3.84 - 0.68 Manchester United
+Liverpool 2.49 - 0.87 West Ham
+Liverpool 2.62 - 0.63 Aston Villa
+Liverpool 2.02 - 1.07 Brentford
+Liverpool 2.4 - 1.72 Brighton
+Liverpool 2.02 - 0.83 Chelsea
+Liverpool 2.53 - 1.62 Manchester City
+Liverpool 3.34 - 0.74 Nottingham Forest
+Liverpool 3.61 - 0.67 Southampton
+Liverpool 3.29 - 0.34 Wolves
+Liverpool 1.97 - 1.86 Arsenal
+Manchester City 2.4 - 0.78 Crystal Palace
+Manchester City 1.82 - 0.99 Fulham
+Manchester City 3.65 - 0.75 Bournemouth
+Manchester City 2.3 - 1.05 Leeds
+Manchester City 3.55 - 1.46 Leicester
+Manchester City 1.6 - 1.22 Newcastle
+Manchester City 3.41 - 1.59 Tottenham
+Manchester City 1.89 - 0.92 Everton
+Manchester City 3.69 - 0.96 Manchester United
+Manchester City 2.72 - 0.81 West Ham
+Manchester City 2.71 - 0.8 Aston Villa
+Manchester City 1.96 - 1.48 Brentford
+Manchester City 2.29 - 1.75 Brighton
+Manchester City 2.01 - 0.74 Chelsea
+Manchester City 2.99 - 1.34 Liverpool
+Manchester City 3.36 - 0.53 Nottingham Forest
+Manchester City 3.41 - 0.75 Southampton
+Manchester City 3.36 - 0.45 Wolves
+Manchester City 2.08 - 1.87 Arsenal
+Nottingham Forest 1.28 - 1.05 Crystal Palace
+Nottingham Forest 1.34 - 1.55 Fulham
+Nottingham Forest 1.75 - 1.63 Bournemouth
+Nottingham Forest 2.13 - 1.05 Leeds
+Nottingham Forest 1.67 - 1.17 Leicester
+Nottingham Forest 1.25 - 1.57 Newcastle
+Nottingham Forest 1.27 - 1.38 Tottenham
+Nottingham Forest 1.79 - 1.51 Everton
+Nottingham Forest 1.55 - 1.19 Manchester United
+Nottingham Forest 1.23 - 1.13 West Ham
+Nottingham Forest 1.77 - 1.42 Aston Villa
+Nottingham Forest 1.54 - 1.31 Brentford
+Nottingham Forest 1.98 - 1.19 Brighton
+Nottingham Forest 1.31 - 1.15 Chelsea
+Nottingham Forest 1.57 - 0.93 Liverpool
+Nottingham Forest 1.08 - 1.12 Manchester City
+Nottingham Forest 2.28 - 1.63 Southampton
+Nottingham Forest 1.95 - 1.44 Wolves
+Nottingham Forest 1.29 - 1.11 Arsenal
+Southampton 0.6 - 1.42 Crystal Palace
+Southampton 0.46 - 1.78 Fulham
+Southampton 0.58 - 1.29 Bournemouth
+Southampton 1.74 - 1.5 Leeds
+Southampton 1.46 - 1.66 Leicester
+Southampton 1.32 - 2.87 Newcastle
+Southampton 1.75 - 2.45 Tottenham
+Southampton 1.18 - 1.62 Everton
+Southampton 1.25 - 1.81 Manchester United
+Southampton 0.97 - 1.88 West Ham
+Southampton 0.89 - 1.42 Aston Villa
+Southampton 0.71 - 1.64 Brentford
+Southampton 1.52 - 2.53 Brighton
+Southampton 1.44 - 1.49 Chelsea
+Southampton 2.06 - 2.34 Liverpool
+Southampton 0.83 - 2.67 Manchester City
+Southampton 0.84 - 1.2 Nottingham Forest
+Southampton 1.22 - 1.52 Wolves
+Southampton 0.87 - 2.19 Arsenal
+Wolves 1.17 - 0.55 Crystal Palace
+Wolves 0.55 - 1.24 Fulham
+Wolves 0.66 - 0.64 Bournemouth
+Wolves 1.84 - 2.13 Leeds
+Wolves 0.91 - 2.23 Leicester
+Wolves 0.99 - 1.81 Newcastle
+Wolves 1.02 - 1.49 Tottenham
+Wolves 1.42 - 1.07 Everton
+Wolves 1.01 - 1.3 Manchester United
+Wolves 0.96 - 0.87 West Ham
+Wolves 1.33 - 0.83 Aston Villa
+Wolves 0.58 - 1.47 Brentford
+Wolves 1.18 - 1.91 Brighton
+Wolves 1.01 - 1.55 Chelsea
+Wolves 0.93 - 1.35 Liverpool
+Wolves 0.44 - 1.66 Manchester City
+Wolves 1.19 - 0.37 Nottingham Forest
+Wolves 1.28 - 0.47 Southampton
+Wolves 0.54 - 1.87 Arsenal
+Arsenal 2.72 - 1.07 Crystal Palace
+Arsenal 2.02 - 1.05 Fulham
+Arsenal 3.71 - 1.15 Bournemouth
+Arsenal 2.3 - 1.18 Leeds
+Arsenal 3.49 - 1.39 Leicester
+Arsenal 1.32 - 0.97 Newcastle
+Arsenal 3.3 - 1.32 Tottenham
+Arsenal 2.43 - 0.92 Everton
+Arsenal 3.75 - 1.07 Manchester United
+Arsenal 2.73 - 0.94 West Ham
+Arsenal 2.82 - 0.92 Aston Villa
+Arsenal 2.12 - 1.38 Brentford
+Arsenal 2.16 - 1.84 Brighton
+Arsenal 2.23 - 0.95 Chelsea
+Arsenal 2.55 - 1.3 Liverpool
+Arsenal 2.43 - 1.68 Manchester City
+Arsenal 3.57 - 0.75 Nottingham Forest
+Arsenal 3.31 - 1.32 Southampton
+Arsenal 3.58 - 0.62 Wolves`
+
+let teams = ['Crystal Palace', 'Fulham', 'Bournemouth', 'Leeds', 'Leicester', 'Newcastle',
+                   'Tottenham', 'Everton', 'Manchester United', 'West Ham', 'Arsenal',
+                   'Aston Villa', 'Brentford', 'Brighton', 'Chelsea', 'Liverpool',
+                   'Manchester City', 'Nottingham Forest', 'Southampton', 'Wolves'];
+
+teams = teams.join(', ');
+
+function same_start(string_to_compare, string) {
+  for (i=0; i < string_to_compare.length; i++) {
+    if (string[i] != string_to_compare[i] ) {
+      return false
+    }
+  }
+    return true
+}
+
+function findMatch(homeTeam, awayTeam) {
+    const matchs = data.split('\n');
+    for (const game of matchs) {
+      if (game.includes(homeTeam) && game.includes(awayTeam) && same_start(homeTeam, game)) {
+        return game;
+      }
+    }
+}
+
+console.log(findMatch("Manchester City", "Manchester United"));
+
+function printScore() {
+  const homeTeamSelect = document.getElementById('homeTeam');
+  const awayTeamSelect = document.getElementById('awayTeam');
+  const homeTeam = homeTeamSelect.value;
+  const awayTeam = awayTeamSelect.value;
+
+  console.log("Selected Home Team:", homeTeam);
+  console.log("Selected Away Team:", awayTeam);
+
+  const result = document.getElementById('result');
+  
+  if (homeTeam === awayTeam) {
+    result.textContent = "Invalid input"
+  }
+  else {
+    const score = findMatch(homeTeam, awayTeam); 
+    result.textContent = score;
+}
+  }
+  
+
